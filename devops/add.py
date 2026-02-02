b7cf2298b22563a88bc60b44e111a72d8fd28ef5
niharika <niharika.mangalpalli@cumminscollege.in>
2026-02-02 12:06:36 +0530

    addition

---
+++ devops/add.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+def main():
+    a=int(input("Enter first number"))
+    b=int(input("Enter second number"))
+    print(addition(a,b))
+if __name__=="__main__":
+    main()
